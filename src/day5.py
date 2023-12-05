bb34d4ab6f395270928d58b8e12260492641a3ae
WHEOfficial <46579241+WHEOfficial@users.noreply.github.com>
2023-12-05 12:22:16 -0500

    Day 5 Part 2 brute force attempt (failed)

--- src/day5.py
+++ src/day5.py
@@ -1,4 +1,17 @@
+from multiprocessing import Pool
+from functools import partial
 from lib import *
+
+def naive_ass_solution(maps, seed):
+    #print(seed, maps)
+    current_value = seed
+    for map_group in maps:
+        for m in map_group:
+            dest, source, length = m
+            if current_value in range(source, source + length):
+                current_value = dest + (current_value - source)
+                break
+    return current_value
 
 def part1(data):
     seeds = [int(i) for i in data[0].split()[1:]]
@@ -17,14 +30,7 @@ def part1(data):
     
     location_list = []
     for seed in seeds:
-        current_value = seed
-        for map_group in maps:
-            for m in map_group:
-                dest, source, length = m
-                if current_value in range(source, source + length):
-                    current_value = dest + (current_value - source)
-                    break
-        location_list.append(current_value)
+        location_list.append(naive_ass_solution(maps, seed))
     return min(location_list)
 
 def part2(data):
@@ -41,25 +47,44 @@ def part2(data):
             one_map = [int(i) for i in data[line].split()]
             maps[map_index].append(one_map)
         line += 1
-    
+
+    seed_ranges = []
     for i in range(0, len(seeds), 2):
         seed, seed_length = seeds[i], seeds[i + 1]
         seed_range = range(seed, seed + seed_length)
-        current_ranges = [seed_range]
+        seed_ranges.append(seed_range)
+    
+    p = Pool()
+    location_list = []
+    for i, seed_range in enumerate(seed_ranges):
+        print(f"Starting range {i + 1}/{len(seed_ranges)} ({seed_range})")
+        answer = p.map(partial(naive_ass_solution, maps), seed_range)
+        location_list.append(min(answer))
+        print(f"Completed range {i + 1}/{len(seed_ranges)}")
+    return min(location_list)
+    
+    # for i in range(0, len(seeds), 2):
+    #     seed, seed_length = seeds[i], seeds[i + 1]
+    #     seed_range = range(seed, seed + seed_length)
+    #     current_ranges = [seed_range]
         
-        for map_group in maps:
-            new_ranges = []
-            for r in current_ranges:
-                num = r.start
-                while num < r.stop:
-                    for m in map_group:
-                        dest, source, length = m
-                        source_range = range(source, source + length)
-                        if num in source_range:
-                            new_range = dest + (num - source)
-                            
-
-
+        # for map_group in maps:
+        #     print(current_ranges)
+        #     new_ranges = []
+        #     for r in current_ranges:
+        #         num = r.start
+        #         while num < r.stop:
+        #             for m in map_group:
+        #                 dest, source, length = m
+        #                 source_range = range(source, source + length)
+        #                 if num in source_range:
+        #                     new_range = dest + (num - source)
+        #                     if (source + length) - 1 in r:
+        #                         new_range = range(new_range, source + length)
+        #                         new_ranges.append(new_range)
+        #                         num = source + length
+        #                         break
+        #                     else:
 
 
 def main():
